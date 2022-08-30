152e3058cf7e6f38608ceadabaf1997e54b07dc9
Nikitas Tsiris <nikitastsir@gmail.com>
2022-08-30 18:34:15 +0000

    Fixed problem with metallb not giving external IPs to LoadBalancer services

--- scripts/setup_master_node.sh
+++ scripts/setup_master_node.sh
@@ -15,16 +15,17 @@ curl https://docs.projectcalico.org/manifests/calico.yaml -O
 kubectl apply -f calico.yaml
 
 #! Configure cluster for metallb installation
+#! Latest metallb version 13.4 does not give external IPs to LoadBalancers
 kubectl get configmap kube-proxy -n kube-system -o yaml | \
 sed -e "s/strictARP: false/strictARP: true/" | \
 kubectl apply -f - -n kube-system
 
-#! Install metallb
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.4/config/manifests/metallb-native.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.4/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.4/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 kubectl apply -f $ROOT/configs/metallb/metallb-configmap.yaml
 
-# Install and configure istio:
+#! Install and configure istio:
 cd $ROOT
 curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.14.3 TARGET_ARCH=x86_64 sh -
 export PATH=$PATH:$ROOT/istio-1.14.3/bin
