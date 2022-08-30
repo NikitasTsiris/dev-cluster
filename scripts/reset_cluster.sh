188089c7e04f9dc8f8153b211ced7a7676344d94
Nikitas Tsiris <nikitastsir@gmail.com>
2022-08-30 19:11:30 +0300

    typo in cri-socket format

--- scripts/reset_cluster.sh
+++ scripts/reset_cluster.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT="$( cd $DIR && cd .. && pwd)"
 SCRIPTS=$ROOT/scripts
 
-sudo kubeadm reset --cri-socket=unix://run/containerd/containerd.sock -f
+sudo kubeadm reset --cri-socket unix:///run/containerd/containerd.sock -f
 
 ifconfig -a | grep _tap | cut -f1 -d":" | while read line ; do sudo ip link delete "$line" ; done
 ifconfig -a | grep tap_ | cut -f1 -d":" | while read line ; do sudo ip link delete "$line" ; done
