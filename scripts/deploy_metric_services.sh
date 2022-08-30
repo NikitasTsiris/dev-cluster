bb82faec87157caf9163483128aab6ef1b09e35f
Nikitas Tsiris <nikitastsir@gmail.com>
2022-08-31 00:21:17 +0300

    Fixed deployment of metric services

--- scripts/deploy_metric_services.sh
+++ scripts/deploy_metric_services.sh
@@ -4,6 +4,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT="$( cd $DIR && cd .. && pwd)"
 SCRIPTS=$ROOT/scripts
 
+#! Needed in order to expose the metrics' services
+INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+INGRESS_DOMAIN=${INGRESS_HOST}.nip.io
 
 #! Deploy metric tools:
 # -> Prometheus
@@ -13,13 +16,205 @@ SCRIPTS=$ROOT/scripts
 kubectl apply -f $ROOT/istio-1.14.3/samples/addons/
 
 #! Expose the metric services to be access via istio ingress gateway
-kubectl apply -f $ROOT/configs/kiali/expose-kiali.yaml
-kubectl apply -f $ROOT/configs/grafana/expose-grafana.yaml
-kubectl apply -f $ROOT/configs/prometheus/expose-prometheus.yaml
-kubectl apply -f $ROOT/configs/tracing/expose-tracing.yaml
+# kubectl apply -f $ROOT/configs/kiali/expose-kiali.yaml
+# kubectl apply -f $ROOT/configs/grafana/expose-grafana.yaml
+# kubectl apply -f $ROOT/configs/prometheus/expose-prometheus.yaml
+# kubectl apply -f $ROOT/configs/tracing/expose-tracing.yaml
 
 #! Metrics can be accessed via:
 #* Kiali: http://kiali.${INGRESS_DOMAIN}
 #* Prometheus: http://prometheus.${INGRESS_DOMAIN}
 #* Grafana: http://grafana.${INGRESS_DOMAIN}
 #* Tracing: http://tracing.${INGRESS_DOMAIN}
+
+#! Grafana:
+cat <<EOF | kubectl apply -f -
+apiVersion: networking.istio.io/v1alpha3
+kind: Gateway
+metadata:
+  name: grafana-gateway
+  namespace: istio-system
+spec:
+  selector:
+    istio: ingressgateway
+  servers:
+  - port:
+      number: 80
+      name: http-grafana
+      protocol: HTTP
+    hosts:
+    - "grafana.${INGRESS_DOMAIN}"
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
+metadata:
+  name: grafana-vs
+  namespace: istio-system
+spec:
+  hosts:
+  - "grafana.${INGRESS_DOMAIN}"
+  gateways:
+  - grafana-gateway
+  http:
+  - route:
+    - destination:
+        host: grafana
+        port:
+          number: 3000
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: DestinationRule
+metadata:
+  name: grafana
+  namespace: istio-system
+spec:
+  host: grafana
+  trafficPolicy:
+    tls:
+      mode: DISABLE
+---
+EOF
+
+#! Kiali:
+cat <<EOF | kubectl apply -f -
+apiVersion: networking.istio.io/v1alpha3
+kind: Gateway
+metadata:
+  name: kiali-gateway
+  namespace: istio-system
+spec:
+  selector:
+    istio: ingressgateway
+  servers:
+  - port:
+      number: 80
+      name: http-kiali
+      protocol: HTTP
+    hosts:
+    - "kiali.${INGRESS_DOMAIN}"
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
+metadata:
+  name: kiali-vs
+  namespace: istio-system
+spec:
+  hosts:
+  - "kiali.${INGRESS_DOMAIN}"
+  gateways:
+  - kiali-gateway
+  http:
+  - route:
+    - destination:
+        host: kiali
+        port:
+          number: 20001
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: DestinationRule
+metadata:
+  name: kiali
+  namespace: istio-system
+spec:
+  host: kiali
+  trafficPolicy:
+    tls:
+      mode: DISABLE
+---
+EOF
+
+#! Prometheus:
+cat <<EOF | kubectl apply -f -
+apiVersion: networking.istio.io/v1alpha3
+kind: Gateway
+metadata:
+  name: prometheus-gateway
+  namespace: istio-system
+spec:
+  selector:
+    istio: ingressgateway
+  servers:
+  - port:
+      number: 80
+      name: http-prom
+      protocol: HTTP
+    hosts:
+    - "prometheus.${INGRESS_DOMAIN}"
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
+metadata:
+  name: prometheus-vs
+  namespace: istio-system
+spec:
+  hosts:
+  - "prometheus.${INGRESS_DOMAIN}"
+  gateways:
+  - prometheus-gateway
+  http:
+  - route:
+    - destination:
+        host: prometheus
+        port:
+          number: 9090
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: DestinationRule
+metadata:
+  name: prometheus
+  namespace: istio-system
+spec:
+  host: prometheus
+  trafficPolicy:
+    tls:
+      mode: DISABLE
+---
+EOF
+
+#! Tracing:
+cat <<EOF | kubectl apply -f -
+apiVersion: networking.istio.io/v1alpha3
+kind: Gateway
+metadata:
+  name: tracing-gateway
+  namespace: istio-system
+spec:
+  selector:
+    istio: ingressgateway
+  servers:
+  - port:
+      number: 80
+      name: http-tracing
+      protocol: HTTP
+    hosts:
+    - "tracing.${INGRESS_DOMAIN}"
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
+metadata:
+  name: tracing-vs
+  namespace: istio-system
+spec:
+  hosts:
+  - "tracing.${INGRESS_DOMAIN}"
+  gateways:
+  - tracing-gateway
+  http:
+  - route:
+    - destination:
+        host: tracing
+        port:
+          number: 80
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: DestinationRule
+metadata:
+  name: tracing
+  namespace: istio-system
+spec:
+  host: tracing
+  trafficPolicy:
+    tls:
+      mode: DISABLE
+---
+EOF
--- scripts/istio-ingress-env.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-#! Needed in order to expose the metrics' services
-INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-INGRESS_DOMAIN=${INGRESS_HOST}.nip.io
