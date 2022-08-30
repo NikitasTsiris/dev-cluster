ef1e3b17dadfc61189dbb043abeb743cd677579b
Nikitas Tsiris <nikitastsir@gmail.com>
2022-08-30 23:44:34 +0300

    nothing

--- scripts/deploy_metric_services.sh
+++ scripts/deploy_metric_services.sh
@@ -12,10 +12,6 @@ SCRIPTS=$ROOT/scripts
 # -> Jaeger
 kubectl apply -f $ROOT/istio-1.14.3/samples/addons/
 
-#! Export the variables needed by metrics in ordered to be accessed by the
-#! following yaml files:
-source $SCRIPTS/ingress_env_variables.sh
-
 #! Expose the metric services to be access via istio ingress gateway
 kubectl apply -f $ROOT/configs/kiali/expose-kiali.yaml
 kubectl apply -f $ROOT/configs/grafana/expose-grafana.yaml
