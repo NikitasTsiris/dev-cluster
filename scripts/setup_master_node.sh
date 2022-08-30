808e5fd51d106a42b30046c96c9f705773e6e16c
Nikitas Tsiris <nikitastsir@gmail.com>
2022-08-30 16:02:48 +0300

    Changed metallb config map

--- scripts/setup_master_node.sh
+++ scripts/setup_master_node.sh
@@ -29,4 +29,4 @@ cd $ROOT
 curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.14.3 TARGET_ARCH=x86_64 sh -
 export PATH=$PATH:$ROOT/istio-1.14.3/bin
 sudo sh -c  "echo 'export PATH=\$PATH:$ROOT/istio-1.14.3/bin' >> /etc/profile"
-istioctl install
+istioctl install --set profile=default -y
