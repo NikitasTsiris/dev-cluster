7f6575369e7504bfc45b874d12a4b5aa03a13973
Nikitas Tsiris <nikitastsir@gmail.com>
2023-02-16 18:08:38 +0200

    fixed path at create_workload  script for social network benchmark

--- socialnetwork/create_workload.sh
+++ socialnetwork/create_workload.sh
@@ -2,10 +2,9 @@
 
 ROOT=$1
 
-cd $ROOT/wrk2
+cd $ROOT
 
-make
 
-./wrk -D exp -t 10 -c 5 -d 600 -L -s ./scripts/social-network/compose-post.lua http://192.168.121.240:8080/wrk2-api/post/compose -R 100 &
-./wrk -D exp -t 10 -c 5 -d 600 -L -s ./scripts/social-network/read-home-timeline.lua http://192.168.121.240:8080/wrk2-api/home-timeline/read -R 100 &
-./wrk -D exp -t 10 -c 5 -d 600 -L -s ./scripts/social-network/read-user-timeline.lua http://192.168.121.240:8080/wrk2-api/user-timeline/read -R 100
+../wrk2/wrk -D exp -t 10 -c 50 -d 6 -L -s ./wrk2/scripts/social-network/compose-post.lua http://10.96.12.240:8080/wrk2-api/post/compose -R 100 &
+../wrk2/wrk -D exp -t 10 -c 50 -d 6 -L -s ./wrk2/scripts/social-network/read-home-timeline.lua http://10.96.12.240:8080/wrk2-api/home-timeline/read -R 100 &
+../wrk2/wrk -D exp -t 10 -c 50 -d 6 -L -s ./wrk2/scripts/social-network/read-user-timeline.lua http://10.96.12.240:8080/wrk2-api/user-timeline/read -R 100
